90bc89ad9829a5c16844e361315853ebb4301b90
Thib-echo <thibault.bac@echoproject.fr>
2023-12-05 15:48:50 +0100

    [TRANSCRIPTION] - Add dump of all segments data from faster_whisper

--- FasterWhisperTranscription.py
+++ FasterWhisperTranscription.py
@@ -16,10 +16,35 @@ class FasterWhisperTranscription:
     @staticmethod
     def format_faster_whisper_output(faster_whisper_output):
         # Concatenate all transcriptions into one string
-        concatenated_transcription = ' '.join(segment.text for segment in faster_whisper_output)
-        return concatenated_transcription
+        segments_data = []
+
+        for segment in faster_whisper_output:
+            segment_data = {
+                "id": segment.id,
+                "seek": segment.seek,
+                "start": segment.start,
+                "end": segment.end,
+                "text": segment.text,
+                "tokens": segment.tokens,
+                "temperature": segment.temperature,
+                "avg_logprob": segment.avg_logprob,
+                "compression_ratio": segment.compression_ratio,
+                "no_speech_prob": segment.no_speech_prob,
+                "words": [
+                    {
+                        "start": word.start,
+                        "end": word.end,
+                        "word": word.word,
+                        "probability": word.probability
+                    } for word in segment.words
+                ]
+            }
+            segments_data.append(segment_data)
+
+        concatenated_transcription = ' '.join(segment['text'] for segment in segments_data)
+        return concatenated_transcription, segments_data
 
     def transcribe(self):
         print(f"Starting transcription for FasterWhisper-{self.model_size}")
-        segments, _ = self.model.transcribe(self.audio_path, beam_size=5)
+        segments, _ = self.model.transcribe(self.audio_path, beam_size=5, vad_filter=True, word_timestamps=True)
         return self.format_faster_whisper_output(segments)
--- transcribe_all.py
+++ transcribe_all.py
@@ -2,13 +2,14 @@ from FasterWhisperTranscription import FasterWhisperTranscription
 from pathlib import Path
 from mutagen.mp3 import MP3
 
+import json
 import time
 import shutil
 import argparse
 
 def transcribe_and_time(transcription_service, audio_duration):
     start_time = time.time()
-    transcription = transcription_service.transcribe()
+    transcription, segments_data = transcription_service.transcribe()
     end_time = time.time()
 
     transcription_time = end_time - start_time
@@ -16,7 +17,7 @@ def transcribe_and_time(transcription_service, audio_duration):
 
     print(f"{transcription_service.__class__.__name__}-{transcription_service.model_size} Transcription Time: {transcription_time:.2f} seconds ({percentage:.2f}% of audio duration)")
     
-    return transcription, transcription_time
+    return transcription, segments_data, transcription_time
 
 def get_audio_duration_from_metadata(audio_path):
     audio_file = Path(audio_path)
@@ -48,19 +49,23 @@ def process_audio_files(folder_path, new_folder):
 
         # Transcribe the audio file
         faster_whisper = FasterWhisperTranscription(str(audio_path), "large-v3")
-        transcription_fw, _ = transcribe_and_time(faster_whisper, audio_duration)
+        transcription_fw, segments_data, _ = transcribe_and_time(faster_whisper, audio_duration)
 
         # Create a new folder for the audio and its transcript
         new_folder_path = new_folder / audio_path.stem
         new_folder_path.mkdir(exist_ok=True)
 
         # Move the audio file to the new folder
-        shutil.move(str(audio_path), str(new_folder_path / audio_path.name))
+        shutil.copy(str(audio_path), str(new_folder_path / audio_path.name))
 
         # Write the transcript to a text file in the new folder
         transcript_file_path = new_folder_path / (audio_path.stem + "_transcription.txt")
         with open(transcript_file_path, "w") as transcript_file:
             transcript_file.write(transcription_fw)
+
+        transcript_json_path = new_folder_path / (audio_path.stem + "_segments_data.json")
+        with open(transcript_json_path, "w") as transcript_json:
+            json.dump(segments_data, transcript_json, indent=4, ensure_ascii=False)
 
         print(f"Processed and moved {audio_path.name}")
 
